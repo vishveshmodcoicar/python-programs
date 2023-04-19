b054f2ecb3e0bd0ce5251e70211e46c09c2b4652
vishveshmodcoicar <118298274+vishveshmodcoicar@users.noreply.github.com>
2023-04-19 12:43:08 +0530

    simple arithmetic based program

---
+++ After 2 Years Age Calculator Program.py
@@ -0,0 +1,5 @@
+#Author : Vishvesh Modcoicar
+fname=input("Enter First Name:")
+lname=input("Enter Last Name:")
+age=int(input("Enter Your Age:"))
+print("After 2 Years:",fname,lname,"Will be",age+2)
